3a04311e2b27146c9dacd94efc3d22ecdf817d9e
JGSims <josi3006@hotmail.com>
2020-04-30 12:32:35 -0400

    fixed student list

--- client/src/components/studentList.js
+++ client/src/components/studentList.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 import Modal from "./modal";
-import { auth } from "../service/firebase";
+import { auth } from "../services/firebase";
 import { db } from "../services/firebase";
 
 import "./studentList.css";
