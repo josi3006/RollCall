3a6fe748ce33096e62b7eda3ba900205263957f3
zsadov1 <zsadov1@gmail.com>
2020-04-30 17:06:57 -0400

    Footstyle/logout button/hover cursor

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -1,16 +1,29 @@
-import React from 'react';
-import { auth } from '../services/firebase';
+import React from "react";
+import { auth } from "../services/firebase";
 
+import "./footer.css";
 
 function Footer() {
-  return (
-    <footer>
-      <div className="footer-copyright">
-        <p><small>&copy; Sadovszky &amp; Sims 2020</small></p>
-      </div>
-      <button className="btn btn-primary mr-3" onClick={() => auth().signOut()}>Logout</button>
-    </footer>
-  )
+	return (
+		<>
+			<footer class='transparent z-depth-0 sticky-footer'>
+				<div class='row'>
+					<div class='col s12'>
+						<div className='footercontainer'>
+							<div
+								className='center addbutton'
+								onClick={() => auth().signOut()}>
+								Logout
+							</div>
+						</div>
+						<p className='center copyright'>
+							<medium>&copy; Sadovszky &amp; Sims 2020</medium>
+						</p>
+					</div>
+				</div>
+			</footer>
+		</>
+	);
 }
 
 export default Footer;
--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -1,120 +1,117 @@
 import React, { Component } from "react";
-// import { Link } from "react-router-dom";
 import { signin } from "../helpers/auth";
+
 import Logo from "../components/logo";
-// import Footer from "../components/Footer"
-import "./login.css"
+
+import "./login.css";
 
 export default class Login extends Component {
-  constructor() {
-    super();
-    this.state = {
-      error: null,
-      email: "",
-      password: ""
-    };
-    this.handleChange = this.handleChange.bind(this);
-    this.handleSubmit = this.handleSubmit.bind(this);
-    // this.googleSignIn = this.googleSignIn.bind(this);
-    // this.githubSignIn = this.githubSignIn.bind(this);
-  }
+	constructor() {
+		super();
+		this.state = {
+			error: null,
+			email: "",
+			password: "",
+		};
+		this.handleChange = this.handleChange.bind(this);
+		this.handleSubmit = this.handleSubmit.bind(this);
+		// this.googleSignIn = this.googleSignIn.bind(this);
+		// this.githubSignIn = this.githubSignIn.bind(this);
+	}
 
-  handleChange(event) {
-    this.setState({
-      [event.target.name]: event.target.value
-    });
-  }
+	handleChange(event) {
+		this.setState({
+			[event.target.name]: event.target.value,
+		});
+	}
 
-  async handleSubmit(event) {
-    event.preventDefault();
-    this.setState({ error: "" });
-    try {
-      await signin(this.state.email, this.state.password);
-    } catch (error) {
-      this.setState({ error: error.message });
-    }
-  }
+	async handleSubmit(event) {
+		event.preventDefault();
+		this.setState({ error: "" });
+		try {
+			await signin(this.state.email, this.state.password);
+		} catch (error) {
+			this.setState({ error: error.message });
+		}
+	}
 
-  // async googleSignIn() {
-  //   try {
-  //     await signInWithGoogle();
-  //   } catch (error) {
-  //     this.setState({ error: error.message });
-  //   }
-  // }
+	// async googleSignIn() {
+	//   try {
+	//     await signInWithGoogle();
+	//   } catch (error) {
+	//     this.setState({ error: error.message });
+	//   }
+	// }
 
-  // async githubSignIn() {
-  //   try {
-  //     await signInWithGitHub();
-  //   } catch (error) {
-  //     this.setState({ error: error.message });
-  //   }
-  // }
+	// async githubSignIn() {
+	//   try {
+	//     await signInWithGitHub();
+	//   } catch (error) {
+	//     this.setState({ error: error.message });
+	//   }
+	// }
 
-  render() {
-    return (
-      <div className="container">
-        <form
-          className="mt-5 py-5 px-5"
-          autoComplete="off"
-          onSubmit={this.handleSubmit}
-        >
-          <Logo />
+	render() {
+		return (
+			<div className='container'>
+				<form
+					className='mt-5 py-5 px-5'
+					autoComplete='off'
+					onSubmit={this.handleSubmit}>
+					<Logo />
 
-          <div className="row">
+					<div className='row'>
+						<div className='col s12'>
+							<div className='form-group'>
+								<input
+									className='form-control'
+									placeholder='Email'
+									name='email'
+									type='email'
+									onChange={this.handleChange}
+									value={this.state.email}
+								/>
+							</div>
+						</div>
+					</div>
 
-            <div className="col s12">
-              <div className="form-group">
-                <input
-                  className="form-control"
-                  placeholder="Email"
-                  name="email"
-                  type="email"
-                  onChange={this.handleChange}
-                  value={this.state.email}
-                />
-              </div>
-            </div>
-          </div>
+					<div className='row'>
+						<div className='col s12'>
+							<div className='form-group'>
+								<input
+									className='form-control'
+									placeholder='Password'
+									name='password'
+									onChange={this.handleChange}
+									value={this.state.password}
+									type='password'
+								/>
+							</div>
+						</div>
+					</div>
 
-          <div className="row">
-
-            <div className="col s12">
-
-              <div className="form-group">
-                <input
-                  className="form-control"
-                  placeholder="Password"
-                  name="password"
-                  onChange={this.handleChange}
-                  value={this.state.password}
-                  type="password"
-                />
-              </div>
-            </div>
-          </div>
-
-          <div className="row">
-
-            <div className="col s12">
-              <div className="form-group">
-                {this.state.error ? (
-                  <p className="error-text">{this.state.error}</p>
-                ) : null}
-                <div className="buttoncontainer">
-
-                  <button className="transparent btn waves-effect waves-light" type="submit">Login</button>
-                </div>
-              </div>
-            </div>
-          </div>
-
-        </form>
-
-        <p><small>&copy; Sadovszky &amp; Sims 2020</small></p>
-
-      
-      </div >
-    );
-  }
+					<div className='row'>
+						<div className='col s12'>
+							<div className='form-group'>
+								{this.state.error ? (
+									<p className='error-text'>{this.state.error}</p>
+								) : null}
+								<div className='buttoncontainer'>
+									<button
+										className='transparent btn waves-effect waves-light'
+										type='submit'>
+										Login
+									</button>
+								</div>
+							</div>
+						</div>
+					</div>
+				</form>
+				&nbsp; &nbsp;
+				<p className='center copyright'>
+					<medium>&copy; Sadovszky &amp; Sims 2020</medium>
+				</p>
+			</div>
+		);
+	}
 }
