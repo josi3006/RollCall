d1950bbd05afd1b53ac250cc52e2eca545452480
JGSims <josi3006@hotmail.com>
2020-04-30 12:42:56 -0400

    built the build

---
+++ client/build/precache-manifest.96ec50cf6af44ea1ec13463d55240d49.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "ccc497fecca7e34f242d",
+    "url": "/static/css/main.458c02b9.chunk.css"
+  },
+  {
+    "revision": "ccc497fecca7e34f242d",
+    "url": "/static/js/main.487da8fe.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "de2f8b1a1d11ec1b56af",
+    "url": "/static/js/2.4a5dabfc.chunk.js"
+  },
+  {
+    "revision": "8bfc54e6c0ab5442f7c4f6f106b4aa48",
+    "url": "/index.html"
+  }
+];
