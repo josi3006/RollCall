335211c6bf48380d422ee4919cbcd9ae16947e9a
zsadov1 <zsadov1@gmail.com>
2020-04-30 17:53:49 -0400

    Changes navbar img size and responsiveness

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,9 +1,6 @@
 import React from "react";
 import "./navbar.css";
 import { Link } from "react-router-dom";
-// import { Chat } from "../pages/Chat";
-
-// import { Sidenav } from "materialize-css";
 
 function Navbar() {
 	return (
@@ -12,7 +9,7 @@ function Navbar() {
 				<div class='row'>
 					<div className='col s12'></div>
 					<img
-						// style={{ width: 550 }}
+						style={{ width: 750 }}
 						src='/rollcalllogohorizontal-min.png'
 						alt='Roll Call'
 						className='navlogo responsive-img'
